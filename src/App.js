04965b2db4396b73e0baba4684310b57597ad45d
KFAR <fkhosrow-khavar@carboncure.com>
2022-12-06 08:27:21 -0800

    Adding demo components and internalization

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
+
+function Header() {
+  const {t, i18n} = useTranslation('common');
+  return (
+    <div>
+      <h1>
+        {t(`welcome.title`, {framework: 'React'})}
+      </h1>
+      <h2>
+        {t('welcome.subtitle')}
+      </h2>
+      <p>
+        {t('welcome.body')}
+      </p>
+      <button onClick={() => i18n.changeLanguage('de')}>de</button>
+      <button onClick={() => i18n.changeLanguage('en')}>en</button>
+      <button onClick={() => i18n.changeLanguage('es')}>es</button>
+      <button onClick={() => i18n.changeLanguage('fr')}>fr</button>
+      <button onClick={() => i18n.changeLanguage('ch')}>ch</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,10 +4,44 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next'
+
+import common_en from './translations/en/common.json';
+import common_de from './translations/de/common.json';
+import common_es from './translations/es/common.json';
+import common_fr from './translations/fr/common.json';
+import common_ch from './translations/ch/common.json';
+
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    en: {
+      common: common_en
+    },
+    de: {
+      common: common_de
+    },
+    es: {
+      common: common_es
+    },
+    fr: {
+      common: common_fr
+    },
+    ch: {
+      common: common_ch
+    }
+  }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
   </React.StrictMode>
 );
 
